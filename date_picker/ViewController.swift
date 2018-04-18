7e53e32f10508d3329040ef86973d1bf8c156539
D7703_19 <d7703_19@D7703-19ui-iMac.local>
2018-04-18 12:58:10 +0900

    fail

--- date_picker/ViewController.swift
+++ date_picker/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
         myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in self.updateTime()
         })
     }
-
+/*
     func updateTime() {
         let date = Date()
         let formatter = DateFormatter()
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
             view.backgroundColor = UIColor.red
         }
     }
-    
+ */
     @IBAction func changeDatePicker(_ sender: Any) {
         print("change Date Picker")
         print(myDatePicker.date)
@@ -48,8 +48,38 @@ class ViewController: UIViewController {
     }
     
     @IBAction func stopAlert(_ sender: Any) {
-        view.backgroundColor = UIColor.white
+        
     }
     
-}
+    @objc func updateTime() {
+        
+        let date1 = Date()
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy-MM-dd a HH:mm:ss EEE"
+        let time1 = formatter.string(from: date1)
+        currentTimeLabel.text = time1
+        
+        if timeLabel.text == currentTimeLabel.text {
+            view.backgroundColor = UIColor.red
+            
+            let alertController = UIAlertController(title: "알람", message: "지정된시간입니다.", preferredStyle: UIAlertControllerStyle.alert)
+            
+            let DestructiveAction = UIAlertAction(title: "취소", style: UIAlertActionStyle.destructive) { (result : UIAlertAction) -> Void in
+                print("취소")
+                
+            }
+            
+            let okAction = UIAlertAction(title: "확인", style: UIAlertActionStyle.default) { (result : UIAlertAction) -> Void in
+                print("확인")
+                self.view.backgroundColor = UIColor.white
+                
+            }
+            alertController.addAction(DestructiveAction)
+            alertController.addAction(okAction)
+            
+            self.present(alertController, animated: true, completion: nil)
 
+        }
+    }
+}
